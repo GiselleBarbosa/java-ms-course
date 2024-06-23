14f5a25ba12d63af28546e2cbc07ea855879c7a6
GiselleBarbosa <g88barbosa@gmail.com>
2024-06-23 13:00:39 -0300

    removed Autowired

--- hr-payroll/src/main/java/com/giselle/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/giselle/hrpayroll/services/PaymentService.java
@@ -3,17 +3,18 @@ package com.giselle.hrpayroll.services;
 import com.giselle.hrpayroll.entities.Payment;
 import com.giselle.hrpayroll.entities.Worker;
 import com.giselle.hrpayroll.feingClients.WorkerFeingClient;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PaymentService {
 
-    @Autowired
-    private WorkerFeingClient workerFeingClient;
+    private final WorkerFeingClient workerFeingClient;
+
+    public PaymentService(WorkerFeingClient workerFeingClient) {
+        this.workerFeingClient = workerFeingClient;
+    }
 
     public Payment getPayment(Long workerId, int days) {
-
         Worker worker = workerFeingClient.findById(workerId).getBody();
         return new Payment(worker.getName(), worker.getDailyIncome(), days);
     }
